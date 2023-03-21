cb992a73fa785f49ad1cff202232c6465b74b15f
Milkteaqwq <q712515712q@163.com>
2023-03-21 10:36:37 +0800

    ver1，低技术力

--- src/plugins/HELP.py
+++ src/plugins/HELP.py
@@ -1,4 +1,4 @@
-﻿from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot import on_keyword
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Bot, Event
@@ -9,6 +9,6 @@ catch_str = on_keyword({'/help','菜单','help'})
 @catch_str.handle()
 async def send_msg(bot: Bot, event: Event, state: T_State):
     id = event.get_user_id()
-    msg = "[CQ:at,qq={}]".format(id) + '\n奶茶酱帮助文档链接：https://note.youdao.com/s/TnWDjRH7\nBuilt on Nonebot2'
+    msg = "[CQ:at,qq={}]".format(id) + '\n奶茶酱/Aqualite-bot帮助文档链接：https://note.youdao.com/s/TnWDjRH7\nGithub repository：https://github.com/MilkTeaqwq/AqualiteBot'
 
     await catch_str.finish(Message(f'{msg}'))
